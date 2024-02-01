cfb02366f879d17c01ba57747719b519d7ab1e98
Patricio Parada <paradagpatricio@gmail.com>
2024-02-01 15:28:57 -0300

    move type definitions from App.tsx to a dedicated file

---
+++ src/utils/types.ts
@@ -0,0 +1,24 @@
+type Source = {
+  "original": string;
+  "large2x": string;
+  "large": string;
+  "medium": string;
+  "small": string;
+  "portrait": string;
+  "landscape": string;
+  "tiny": string;
+};
+
+export type Photos = {
+  "id": number;
+  "width": number;
+  "height": number;
+  "url": string;
+  "photographer": string;
+  "photographer_url": string;
+  "photographer_id": number;
+  "avg_color": string;
+  "src": Source;
+  "liked": boolean;
+  "alt": string;
+};
